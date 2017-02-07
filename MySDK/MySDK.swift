2c618164ed20467879cf963c21a3289f4e25ef33
Mobility <mobility@SOHAM.local>
2017-02-07 17:46:11 +0530

    Initial Commit

---
+++ MySDK/MySDK.swift
@@ -0,0 +1,15 @@
+//
+//  MySDK.swift
+//  MySDK
+//
+//  Created by Mobility on 07/02/17.
+//  Copyright © 2017 Mobility. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
